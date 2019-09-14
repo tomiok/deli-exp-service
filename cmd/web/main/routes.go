4e615bfb02fb481b76f6d96ded64c3209a834062
tomaslingotti <tomas.lingotti@pedidosya.com>
2019-09-14 04:20:44 -0300

    WIP

---
+++ cmd/web/main/routes.go
@@ -0,0 +1,10 @@
+package main
+
+import "github.com/go-chi/chi"
+
+func routes(mux *chi.Mux) {
+
+	mux.Route("/experiences", func (router chi.Router) {
+		router. Post("/", createExpHandler)
+	})
+}

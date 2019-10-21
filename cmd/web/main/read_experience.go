5d5c4747f08197ed22e1ee71166ac573e2f84849
tomaslingotti <tomaslingotti@gmail.com>
2019-10-21 00:58:59 -0300

    get url param

--- cmd/web/main/read_experience.go
+++ cmd/web/main/read_experience.go
@@ -1,7 +1,13 @@
 package main
 
-import "net/http"
+import (
+	"github.com/deli/exp-service/commons/logs"
+	"github.com/go-chi/chi"
+	"net/http"
+)
 
 func getExpByIDHandler(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "expId")
 
+	logs.Infof("lookup for exp id %s", id)
 }
